0552f2c8df121ab1a7d8de6076b898756b279975
Gundali <srinivas.gundali@ENTROPIA.GLOBAL>
2019-09-11 19:37:04 +0800

    layout changes reverted

---
+++ src/components/NoSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+ 
+  
+</template>
+<script>
+export default {
+  name: "no-sidebar-component",
+
+  data() {
+    return {
+      
+    };
+  },mounted() {
+    
+  }
+
+  
+};
+</script>
